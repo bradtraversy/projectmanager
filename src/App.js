c2692f1caeceb7b387fdffa5956cbaa7e44529b3
Elias Toivanen <elias.a.toivanen@gmail.com>
2017-03-02 23:18:36 +0200

    Some simplifications for modern browsers
    1. HTTP requests in App.js
     * Prefer the native window.fetch API over jQuery
     * No need to do 2 requests
     * Simpler, aye?
    
    2. Demonstrate how one can delegate props down the component tree with
       the spread operator
      * Works with JSX too
      * No need to wire components explicitly

--- src/App.js
+++ src/App.js
@@ -1,88 +1,105 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import uuid from 'uuid';
-import $ from 'jquery';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import Todos from './Components/Todos';
 import './App.css';
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      projects: [],
-      todos:[]
+const mockTodos = [
+    'https://jsonplaceholder.typicode.com/todos',
+    {
+        'Prama': 'No-Cache',
+        'User-Agent': 'Mocker'
     }
-  }
+];
 
-  getTodos(){
-    $.ajax({
-      url: 'https://jsonplaceholder.typicode.com/todos',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({todos: data}, function(){
-          console.log(this.state);
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            projects: [],
+            todos: []
+        }
+    }
+
+    getTodos() {
+
+        // ditch jQuery, use window fetch API
+
+        let setTodos = todos => {
+            this.setState({
+                todos: todos
+            });
+        };
+
+        window.fetch(...mockTodos)
+            .then(res => res.json())
+            .then(setTodos)
+            .catch(err => console.error(err));
+
+    }
+
+    getProjects() {
+
+        this.setState({
+            projects: [{
+                    id: uuid.v4(),
+                    title: 'Business Website',
+                    category: 'Web Deisgn'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Social App',
+                    category: 'Mobile Development'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Ecommerce Shopping Cart',
+                    category: 'Web Development'
+                }
+            ]
         });
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-      }
-    });
-  }
+    }
 
-  getProjects(){
-    this.setState({projects: [
-      {
-        id:uuid.v4(),
-        title: 'Business Website',
-        category: 'Web Deisgn'
-      },
-      {
-        id:uuid.v4(),
-        title: 'Social App',
-        category: 'Mobile Development'
-      },
-      {
-        id:uuid.v4(),
-        title: 'Ecommerce Shopping Cart',
-        category: 'Web Development'
-      }
-    ]});
-  }
+    componentWillMount() {
+        this.getProjects();
+        this.getTodos();
+    }
 
-  componentWillMount(){
-    this.getProjects();
-    this.getTodos();
-  }
+    componentDidMount() {
+        // this.getTodos(); Duplicate HTTP request
+    }
 
-  componentDidMount(){
-    this.getTodos();
-  }
+    handleAddProject(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({
+            projects: projects
+        });
+    }
 
-  handleAddProject(project){
-    let projects = this.state.projects;
-    projects.push(project);
-    this.setState({projects:projects});
-  }
+    handleDeleteProject(id) {
+        let projects = this.state.projects;
+        let index = projects.findIndex(x => x.id === id);
+        projects.splice(index, 1);
+        this.setState({
+            projects: projects
+        });
+    }
 
-  handleDeleteProject(id){
-    let projects = this.state.projects;
-    let index = projects.findIndex(x => x.id === id);
-    projects.splice(index, 1);
-    this.setState({projects:projects});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <AddProject addProject={this.handleAddProject.bind(this)} />
-        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
-        <hr />
-        <Todos todos={this.state.todos} />
-      </div>
-    );
-  }
+    render() {
+        return ( 
+            <div className="App" >
+                <AddProject addProject = {this.handleAddProject.bind(this)}/> 
+                <Projects projects = {this.state.projects} onDelete = {this.handleDeleteProject.bind(this)}/>
+                <hr />
+                <Todos todos = {this.state.todos}/> 
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
-  deleteProject(id){
-    this.props.onDelete(id);
-  }
 
-  render() {
-    return (
-      <li className="Project">
-        <strong>{this.props.project.title}</strong>: {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
-      </li>
-    );
-  }
+    render() {
+
+        // Everything is immutable and stateless at the deepest level
+
+        const { onDelete, project } = this.props;
+        const { title, category } = project;
+
+        return (
+            <li className="Project">
+            <strong>{title}</strong>: {category} <a href="#" onClick={onDelete}>X</a>
+            </li>
+        );
+    }
 }
 
 ProjectItem.propTypes = {
-  project: React.PropTypes.object,
-  onDelete: React.PropTypes.func
+    project: React.PropTypes.object,
+    onDelete: React.PropTypes.func
 }
 
 export default ProjectItem;
